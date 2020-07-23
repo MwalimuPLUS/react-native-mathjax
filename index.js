eb0acd1ec13afa560a2072f9537d7ab80dbf3afe
Rodney Onyango <onyangorodney@gmail.com>
2020-07-23 22:36:17 +0300

    Update mathjax logic

--- index.js
+++ index.js
@@ -1,74 +1,71 @@
 import React from 'react';
-import { View } from 'react-native';
-import AutoHeightWebView from 'react-native-autoheight-webview'
+import {WebView} from 'react-native-webview';
+import {View, Dimensions} from "react-native";
+import AutoHeightWebView from 'react-native-autoheight-webview';
 
 const defaultOptions = {
-	messageStyle: 'none',
-	extensions: [ 'tex2jax.js' ],
-	jax: [ 'input/TeX', 'output/HTML-CSS' ],
-	tex2jax: {
-		inlineMath: [ ['$','$'], ['\\(','\\)'] ],
-		displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
-		processEscapes: true,
-	},
-	TeX: {
-		extensions: ['AMSmath.js','AMSsymbols.js','noErrors.js','noUndefined.js']
-	}
+    messageStyle: 'none',
+    extensions: ['tex2jax.js'],
+    jax: ['input/TeX', 'output/HTML-CSS'],
+    tex2jax: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        displayMath: [['$$', '$$'], ['\\[', '\\]']],
+        processEscapes: true,
+    },
+    TeX: {
+        extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js']
+    }
 };
 
-class MathJax extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			height: 1
-		};
-	}
+function MathJax(props) {
+    const {html, mathJaxOptions, styles} = props;
 
-	handleMessage(message) {
-		this.setState({
-			height: Number(message.nativeEvent.data)
-		});
-	}
+    //wrap html in math jaxx.
+    function wrapMathjax() {
+        const options = JSON.stringify(
+            Object.assign({}, defaultOptions, mathJaxOptions)
+        );
+        //if not mathjax present then we do not need to use mathjax.
+        if (!html || (html.indexOf('$$') === -1 &&
+            html.indexOf('$') === -1 &&
+            html.indexOf('\(') === -1 &&
+            html.indexOf('\[') === -1)) {
+            return html;
+        } else {
+            return `<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+                <script type="text/x-mathjax-config">
+                    MathJax.Hub.Config(${options});
+                    MathJax.Hub.Queue(function() {
+                        var height = document.documentElement.scrollHeight;
+                        window.postMessage(String(height));
+                        document.getElementById("formula").style.visibility = '';
+                    });
+                </script>
+                <script src="https://mathjax.mwalimuplus.com/MathJax/MathJax.js?config=TeX-MML-AM_CHTML"></script>
+                <p id="formula" style="visibility: hidden;">
+                    ${html}
+			</p>`;
+        }
+    }
 
-	wrapMathjax(content) {
-		const options = JSON.stringify(
-			Object.assign({}, defaultOptions, this.props.mathJaxOptions)
-		);
+    const calculatedHtml = wrapMathjax();
+    // Create new props without `props.html` field. Since it's deprecated.
+    const calculatedProps = Object.assign({}, props, {html: undefined});
+    const calculatedStyles = Object.assign({}, {width: Dimensions.get('window').width}, styles);
 
-		return `
-			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-			<script type="text/x-mathjax-config">
-				MathJax.Hub.Config(${options});
+    function onSizeUpdated(size) {
+    }
 
-				MathJax.Hub.Queue(function() {
-					var height = document.documentElement.scrollHeight;
-
-					window.postMessage(String(height));
-				});
-			</script>
-
-			<script src="https://mathjax.mwalimuplus.com/MathJax/MathJax.js?config=TeX-MML-AM_CHTML"></script>
-
-			${content}
-		`;
-	}
-	render() {
-		const html = this.wrapMathjax(this.props.html);
-
-		// Create new props without `props.html` field. Since it's deprecated.
-		const props = Object.assign({}, this.props, { html: undefined });
-
-		return (
-      <View style={{  ...props.style }}>
+    return (
         <AutoHeightWebView
-          scrollEnabled={false}
-          onMessage={ this.handleMessage.bind(this) }
-          source={{ html }}
-          {...props}
+            style={calculatedStyles}
+            source={{html: calculatedHtml}}
+            scalesPageToFit={true}
+            onSizeUpdated={onSizeUpdated}
+            viewportContent={'width=device-width, user-scalable=yes'}
+            {...calculatedProps}
         />
-      </View>
-		);
-	}
+    )
 }
 
 export default MathJax;
